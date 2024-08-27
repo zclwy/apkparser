5202b910cf0325817fdec38d44aa4f6ddbabed97
张春龙 <zhangcl@2345.com>
2024-08-27 09:06:52 +0800

    feat: 添加sdk相关信息, 调整逻辑层次

--- apk.go
+++ apk.go
@@ -3,6 +3,7 @@ package apkparser
 import (
 	"archive/zip"
 	"bytes"
+	"crypto/md5"
 	"encoding/xml"
 	"errors"
 	"fmt"
@@ -12,6 +13,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // apk is an application package file for android.
@@ -20,6 +22,10 @@ type apk struct {
 	zipReader   *zip.Reader
 	apkManifest apkManifest
 	table       *TableFile
+	supportOs32 bool
+	supportOs64 bool
+	md5         string
+	size        int64
 }
 
 // openFile will open the file specified by filename and return apk
@@ -37,11 +43,12 @@ func openFile(filename string) (apk *apk, err error) {
 		return nil, err
 	}
 	apk.f = f
+	apk.size = fi.Size()
 	return
 }
 
 // openZipReader has same arguments like zip.NewReader
-func openZipReader(r io.ReaderAt, size int64) (*apk, error) {
+func openZipReader(r *os.File, size int64) (*apk, error) {
 	zipReader, err := zip.NewReader(r, size)
 	if err != nil {
 		return nil, err
@@ -55,6 +62,9 @@ func openZipReader(r io.ReaderAt, size int64) (*apk, error) {
 	if err = apk.parseResources(); err != nil {
 		return nil, err
 	}
+	apk.parseOsSupport(zipReader)
+	apk.getApkMd5(r)
+
 	return apk, nil
 }
 
@@ -185,4 +195,68 @@ func (k *apk) readZipFile(name string) (data []byte, err error) {
 	} else {
 		return data, nil
 	}
+}
+
+func (k *apk) parseOsSupport(r *zip.Reader) {
+	var (
+		hasSoFile                bool
+		isSupport32, isSupport64 bool
+	)
+	for _, f := range r.File {
+		if strings.HasSuffix(f.Name, ".so") {
+			hasSoFile = true
+		}
+		// if strings.HasPrefix(f.Name, "lib/arm64-v8a") {
+		// 	fmt.Println(f.Name)
+		// 	isSupport64 = true
+		// }
+		// if strings.HasPrefix(f.Name, "lib/armeabi") {
+		// 	fmt.Println(f.Name)
+		// 	isSupport32 = true
+		// }
+
+		if strings.Contains(f.Name, "armeabi-v7a") || strings.Contains(f.Name, "x86") {
+			isSupport32 = true
+		} else if strings.Contains(f.Name, "arm64-v8a") || strings.Contains(f.Name, "x86_64") {
+			isSupport64 = true
+		}
+	}
+	// 当前apk支持的系统位数
+	if !hasSoFile && !isSupport64 && !isSupport32 {
+		k.supportOs32 = true
+		k.supportOs64 = true
+	} else {
+		k.supportOs32 = isSupport32
+		k.supportOs64 = isSupport64
+	}
+}
+
+// 获取apk md5
+func (k *apk) getApkMd5(file *os.File) {
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return
+	}
+
+	k.md5 = fmt.Sprintf("%032x", hash.Sum(nil))
+}
+
+// 解析apk名称
+func (k *apk) parseApkLabel() string {
+	label, _ := k.label(&ResTableConfig{
+		Language: [2]uint8{'z', 'h'},
+		Country:  [2]uint8{'C', 'N'},
+	})
+	// label, _ := pkg.label(&ResTableConfig{})
+
+	return label
+}
+
+// 解析apk图标
+func (k *apk) parseApkIcon() image.Image {
+	icon, _ := k.icon(&ResTableConfig{
+		Density: 720,
+	})
+
+	return icon
 }
