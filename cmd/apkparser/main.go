f7ddf37de4e694ebe66850dbecc78320afa2e91e
torando <zcllonghere@163.com>
2025-04-17 10:21:13 +0800

    调整脚本参数

--- cmd/apkparser/main.go
+++ cmd/apkparser/main.go
@@ -15,7 +15,11 @@ func main() {
 	}
 
 	apkFile := os.Args[1]
-	app, err := apkparser.New(apkFile)
+	app, err := apkparser.New(apkFile, Option{
+		WithIcon:             true,
+		WithSignature:        true,
+		IgnoreSignatureError: true,
+	})
 	if err != nil {
 		log.Fatalf("Failed to parse APK: %v", err)
 	}
